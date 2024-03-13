3df3920df337922d233b52c85250e963e420c788
Sameer Gitay <sgitay@gmail.com>
2024-03-13 09:20:20 +0530

    sample rect app

---
+++ src/components/Layouts/Header.js
@@ -0,0 +1,25 @@
+import React from "react"
+import "../../styles/HeaderStyle.css"
+
+import {Container, Nav, Navbar}  from 'react-bootstrap';
+
+function Header(){
+    return (
+    <>
+      <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    </>
+    )
+}
+
+export default Header
